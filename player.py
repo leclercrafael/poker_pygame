467a51dd1f2edb569a560dfe9126d7e405de8457
Pierre-Guillaume <pierre-guillaume.petitmangin@etu.minesparis.psl.eu>
2025-10-24 16:01:47 +0200

    Making player class

---
+++ player.py
@@ -0,0 +1,53 @@
+from .hand import Hand 
+from .pot import Pot
+from .flop import Flop
+
+class Player(Hand,Flop) :
+
+    def __init__(self,name : str ,stack : int) -> None :
+        """Initializing the Player """
+        super().__init__()
+        self._name = name
+        self._stack = stack
+
+    def fold(self,B : bool) -> bool:
+        """Checking if the player folds or not"""
+        return(B)
+    
+    def is_raising(self,B : bool) -> bool :
+        """Checking if the player raises or not"""
+        return(B)
+    
+    def raising_price(self,price : int) -> tuple :
+        """Taking the raise of the stack"""
+
+        self._stack -= price
+        return()
+    
+    def is_checking(self, B : bool) -> bool : 
+        """Checking if the player checks or not"""
+        return(B)
+    
+    def best_combinaison(self) -> int :
+        """Calculating the sum of Hand + Flop"""
+        
+    
+
+
+
+
+
+
+
+        
+
+        
+        
+
+        
+
+
+
+
+
+
---
+++ pot.py
@@ -0,0 +1,12 @@
+class Pot() :
+
+    def __init__(self,value : int) -> None: 
+        self._value = value
+        
+
+    def add(self,_raise : int ) -> None :
+        self._value += _raise
+
+    
+
+
