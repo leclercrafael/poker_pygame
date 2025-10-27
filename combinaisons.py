7c645050b39860582e315d5156283c7056d37227
Pierre-Guillaume <pierre-guillaume.petitmangin@etu.minesparis.psl.eu>
2025-10-27 10:03:40 +0100

    add_combinaisons

---
+++ combinaisons.py
@@ -0,0 +1,15 @@
+class Combinaisons ():
+
+    def __init__(self,flop : list, hand : list) -> None :
+        self._flop = flop
+        self._hand = hand
+
+    def one_pair(self):
+        p = 0
+        for i in range (len(self._hand)):
+            card = self._hand.pop()
+            for j in range(len(self._hand))
+                
+
+
+            for card in self
