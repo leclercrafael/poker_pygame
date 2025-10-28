218546e1b276ac45ef2818be2c6312c43d416097
Rafaël Leclerc <rafael.leclerc.rl@gmail.com>
2025-10-28 16:13:45 +0100

    Cards

--- card.py
+++ card.py
@@ -2,7 +2,7 @@ from .game_object import GameObject
 
 class Card(GameObject) :
 
-    def __init__(self,number : int ,color : tuple ) -> None :
+    def __init__(self,number : int, color : tuple ) -> None :
         """Initialize the Object"""
         super().__init__()
         self._number = number
